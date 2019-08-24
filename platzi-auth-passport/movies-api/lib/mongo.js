19a121505c95e0a9f433882d4c2e84dd4e3c2359
MontoyaAndres <andresmontoyafcb@gmail.com>
2019-08-24 11:08:49 -0500

    initial api

---
+++ platzi-auth-passport/movies-api/lib/mongo.js
@@ -0,0 +1,72 @@
+const { MongoClient, ObjectId } = require("mongodb");
+const { config } = require("../config");
+
+const DB_NAME = config.dbName;
+
+const MONGO_URI = "mongodb://localhost:27017/movies";
+
+class MongoLib {
+  constructor() {
+    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
+    this.dbName = DB_NAME;
+  }
+
+  connect() {
+    if (!MongoLib.connection) {
+      MongoLib.connection = new Promise((resolve, reject) => {
+        this.client.connect(err => {
+          if (err) {
+            reject(err);
+          }
+
+          resolve(this.client.db(this.dbName));
+        });
+      });
+    }
+
+    return MongoLib.connection;
+  }
+
+  getAll(collection, query) {
+    return this.connect().then(db => {
+      return db
+        .collection(collection)
+        .find(query)
+        .toArray();
+    });
+  }
+
+  get(collection, id) {
+    return this.connect().then(db => {
+      return db.collection(collection).findOne({ _id: ObjectId(id) });
+    });
+  }
+
+  create(collection, data) {
+    return this.connect()
+      .then(db => {
+        return db.collection(collection).insertOne(data);
+      })
+      .then(result => result.insertedId);
+  }
+
+  update(collection, id, data) {
+    return this.connect()
+      .then(db => {
+        return db
+          .collection(collection)
+          .updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true });
+      })
+      .then(result => result.upsertedId || id);
+  }
+
+  delete(collection, id) {
+    return this.connect()
+      .then(db => {
+        return db.collection(collection).deleteOne({ _id: ObjectId(id) });
+      })
+      .then(() => id);
+  }
+}
+
+module.exports = MongoLib;
